4972276093d99b343e61eb749fc90e729e7eb316
C1ouD-He <hhcbang1@hotmail.com>
2023-10-16 18:24:02 +0800

    Update ip change function.

--- Port_Client.py
+++ Port_Client.py
@@ -2,21 +2,25 @@ import os
 import socket
 import threading
 import asyncio
+import subprocess
 
 class Port_Client(object):
     def __init__(self):
         # 定义服务器地址和端口
         self.HOST = 'localhost'
         self.PORT = 8082
-        # 创建客户端套接字
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((self.HOST, self.PORT))
-        self.onOpened = True
-        self.onConnected = True
+        '''
+            # 创建客户端套接字
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect((self.HOST, self.PORT))
+        '''
+        self.onOpened = False
+        self.onConnected = False
         self.mcu_project = ''
         self.soc_commond = False
         self.input_tmp = ''
         self.echo = False
+        self.run_connecting()
 
     # 删除前一条历史记录的函数
     def delete_previous_history(self):
@@ -35,7 +39,7 @@ class Port_Client(object):
 
     async def receiver(self):
         self.client_socket.settimeout(0.1)
-        print('INFO: Receiver open!')
+        print(f'INFO: Receiver[{self.connName}] open!')
         while self.onOpened:
             try:
                 response = self.client_socket.recv(1024).decode()
@@ -49,16 +53,18 @@ class Port_Client(object):
                         print(response)
             except socket.timeout:
                 pass
+            except ConnectionRefusedError:
+                await self.onConnectFail()
             except OSError:
                 await self.onConnectFail()
-        print('INFO: Receiver close!')
+        print(f'INFO: Receiver[{self.connName}] close!')
 
     def run_receiver(self):
         asyncio.run(self.receiver())
 
     async def sender(self):                                            # send the subscribe message
         self.client_socket.send(self.connName.encode())
-        print(f'INFO: Subscribe {self.connName}!')
+        print(f'INFO: Subscribe[{self.connName}]!')
         self.onOpened = True
         self.thread_rcver = threading.Thread(target=self.run_receiver, daemon=True)
         self.thread_rcver.start()
@@ -95,7 +101,7 @@ class Port_Client(object):
                     self.onOpened = False
                     self.echo = False
                     self.clear_terminal()
-                    print(f'INFO: Unsubscribe {self.connName}!')
+                    print(f'INFO: Unsubscribe[{self.connName}]!')
                     self.client_socket.send(command.encode())   # send the unsubscribe message
                     await asyncio.sleep(0.11)
                     return
@@ -119,27 +125,50 @@ class Port_Client(object):
                     return
             except BrokenPipeError:
                 await self.onConnectFail()
+            except ConnectionRefusedError:
+                await self.onConnectFail()
             except OSError:
                 await self.onConnectFail()
+
             if self.onOpened == False:
                 return
 
     def port_terminal(self):            # index select session
         while True:
-            InputB = input('choose port( 0 ~ 9 | svrlog | quit | h)->')
+            InputB = input('choose ( 0 ~ 9 | svrlog | ipport | chip | chport | quit | h)->')
             if(InputB == 'quit') or (InputB == 'QUIT'):
                 print('INFO: Port Client closed!')
                 exit()
+            elif InputB == 'ipport':
+                print(f'INFO: IP setting: {self.HOST}:{self.PORT}')
+                continue  
+            elif InputB == 'chip':
+                print(f'INFO: IP is {self.HOST}')
+                self.HOST = input('Please enter IP:')
+                print('INFO: IP change success!')
+                print(f'INFO: IP setting: {self.HOST}:{self.PORT}')
+                self.onOpened = False
+                self.onConnected = False
+                self.client_socket.close()
+            elif InputB == 'chport':
+                print(f'INFO: PORT is {self.PORT}')
+                try:
+                    self.PORT = int(input('Please enter PORT:'))
+                    print('INFO: PORT change success!')
+                    print(f'INFO: IP setting: {self.HOST}:{self.PORT}')
+                except ValueError:
+                    print('ERROR: Port num error!')
+                self.onOpened = False
+                self.onConnected = False
+                self.client_socket.close()
             elif InputB == 'h':
                 self.help()
                 continue
-            #if self.onOpened == False and '0' <= InputB <= '9':
-            #    print('Error: Port Server Disconnected!')
             if self.onConnected == False:
                 if InputB != '':
                     self.run_connecting()
             if self.onConnected == True:
-                if InputB == '':
+                if InputB == '' or InputB == 'ipport' or InputB == 'chip' or InputB == 'chport':
                     continue
                 elif InputB == 'svrlog':
                     pass
@@ -155,23 +184,82 @@ class Port_Client(object):
                         continue
                 self.connName = InputB
                 asyncio.run(self.sender())
-                
-                
-        # asyncio.run(self.serial_terminal())
 
-    def port_controller(self):
+
+    def run_port_terminal(self):
         try:
             self.port_terminal()
         except KeyboardInterrupt:
             self.onOpened = False
             self.client_socket.close()
 
+    async def onConnectFail(self):
+        if self.onOpened == False:
+            return
+        self.onOpened = False
+        self.onConnected = False
+        self.client_socket.close()
+        for i in range (5):
+            print(f'Error: Connection failed! ReConnecting{i+1} ...')
+            try:
+                self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.client_socket.connect((self.HOST, self.PORT))
+                self.onOpened = True
+                self.onConnected = True
+                print('INFO: ReConnecting success!')
+                self.thread_rcver = threading.Thread(target=self.run_receiver, daemon=True)     # while reconnect success before back to index page,restart the receiver
+                self.thread_rcver.start()
+                self.client_socket.send(self.connName.encode())
+                return
+            except ConnectionRefusedError:
+                await asyncio.sleep(3)
+            except OSError:
+                await asyncio.sleep(3)
+        print('Error: Connection failed!')
+
+    async def connecting(self):
+        for i in range(5):
+            print(f'INFO: Connecting{i+1}...')
+            try:
+                # del self.client_socket, self.thread_rcver
+                self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.client_socket.settimeout(3)
+                self.client_socket.connect((self.HOST, self.PORT))
+                self.onOpened = True
+                self.onConnected = True
+                print('INFO: Connecting success!')
+                return
+            except socket.timeout:
+                pass
+            except ConnectionRefusedError:
+                await asyncio.sleep(3)
+            except OSError:
+                await asyncio.sleep(3)
+        print('Error: Connection failed!')
+
+    def run_connecting(self):
+        try:
+            asyncio.run(self.connecting())
+        except KeyboardInterrupt:
+            print('\nINFO: Connection broken!')
+        except TypeError:
+            print('ERROR: IP setting error! Please check!')
+        
+    def run(self):
+        self.run_port_terminal()
+
+    def print_help(self, txt):
+        print('\033[;;100m' + txt + '\033[0m')
+
     def help(self):
         self.print_help('|---------------------------------------------------------|')
-        self.print_help('|----------------------#help -v2.1.5----------------------|')
+        self.print_help('|----------------------#help -v2.1.7----------------------|')
         self.print_help('|                                                         |')
         self.print_help('| input 0 ~ 9 to choose ttyUSB0~9                         |')
         self.print_help('| input svrlog to grep server log                         |')
+        self.print_help('| input ipport to show ip setting                         |')
+        self.print_help('| input chip to change ip                                 |')
+        self.print_help('| input chport to change port                             |')
         self.print_help('| input quit to quit the program                          |')
         self.print_help('|                                                         |')
         self.print_help('| when using serial:                                      |')
@@ -181,52 +269,14 @@ class Port_Client(object):
         self.print_help('|                                                         |')
         self.print_help('|---------------------------------------------------------|')
 
-    def print_help(self, txt):
-        print('\033[;;100m' + txt + '\033[0m')
 
-    async def onConnectFail(self):
-        if self.onOpened == False:
-            return
-        self.onOpened = False
-        self.onConnected = False
-        self.client_socket.close()
-        for i in range (5):
-            print(f'Error: Connect failed! ReConnecting{i+1} ...')
-            try:
-                self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.client_socket.connect((self.HOST, self.PORT))
-                self.onOpened = True
-                self.onConnected = True
-                print('INFO: ReConnecting success!')
-                self.thread_rcver = threading.Thread(target=self.run_receiver, daemon=True)
-                self.thread_rcver.start()
-                self.client_socket.send(self.connName.encode())
-                break
-            except OSError:
-                await asyncio.sleep(5)
-
-    async def connecting(self):
-        for i in range(5):
-            print(f'INFO: Connecting{i+1}...')
-            try:
-                # del self.client_socket, self.thread_rcver
-                self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.client_socket.connect((self.HOST, self.PORT))
-                self.onOpened = True
-                self.onConnected = True
-                print('INFO: Connecting success!')
-                break
-            except OSError:
-                await asyncio.sleep(5)
-
-
-    def run_connecting(self):
-        asyncio.run(self.connecting())
-
-    def run(self):
-        self.port_controller()
-
+def run_shell(alias_name):
+    result = subprocess.run(alias_name, shell=True)
+    print(result)
 
 if __name__ == '__main__':
+    # 调用别名为 pwr 的命令
+    # run_shell('sudo /usr/bin/python3.7 /home/pi/.remote_env/ea_psu_ctrl.py')
+
     port_client = Port_Client()
     port_client.run()
